c3ae1d0534688aecee5dfc076eadf957ee4a2daf
Joosep Jääger <joosep.jaager@gmail.com>
2020-07-13 15:40:52 +0300

    Small changes

--- src/TypeInference.hs
+++ src/TypeInference.hs
@@ -34,19 +34,46 @@ data Typing = Typing
 
 data InferenceState = InferenceState
   { _istProg  :: DatalogProgram
-  , _istScope :: [Map String Typing]
   }
+
+type InferenceM = StateT InferenceState (Except InferenceException)
+data TypeSubstitution 
+  = TypeSubstitution (Map String Typing)
+  | SubstitutionFail
 
 makeLenses ''Typing
 makeLenses ''InferenceState
 
-type InferenceM = StateT InferenceState (Except InferenceException)
+instance Semigroup TypeSubstitution where
+  SubstitutionFail <> _ = SubstitutionFail
+  _ <> SubstitutionFail = SubstitutionFail
+  (TypeSubstitution x) <> (TypeSubstitution y) = 
+    TypeSubstitution $ M.unionWith unifier x y
+    where
+      unifier :: Typing -> Typing -> Typing
+      unifier a b = fromMaybe (throw $ TypeMismatch "") $ unifyTypings a b
+
+instance Monoid TypeSubstitution where
+  mempty = TypeSubstitution mempty
+
+applyTypeSubst :: TypeSubstitution -> Rule -> Either String Rule
+applyTypeSubst (TypeSubstitution ts) r = 
+  do
+    r' <- r & ruleHead %%~ f
+    r' & ruleTail %%~ f
+  where
+    t v@(Var _ n) = fromMaybe (exprTyping v) $ M.lookup n ts
+    f v = applyTyping (t v) v
+applyTypeSubst (TypeSubstitution _) x = return x
+
+(|->) :: String -> Typing -> TypeSubstitution
+(|->) x y = TypeSubstitution $ M.singleton x y
 
 evalInferenceM :: InferenceM a -> DatalogProgram -> Either InferenceException a
 evalInferenceM x dp = runExcept $ evalStateT x (defaultState dp)
 
 defaultState :: DatalogProgram -> InferenceState
-defaultState dp = InferenceState dp [M.empty]
+defaultState dp = InferenceState dp
 
 -- Assuming all rules are ground rules and all rule heads are annotated with
 -- binding patterns
@@ -57,44 +84,51 @@ typeInference = evalInferenceM act
       _dp <- use istProg
       _dp & dpRules . traversed %%~ inferRule
 
+-- | Infers types and domains from database facts
 inferRule :: Rule -> InferenceM Rule
 inferRule r =
   do
-    void . traverse f . U.universe $ r ^. ruleTail
-    _scope <- use $ istScope . _head
-    let app v@(Var _ n) = applyTyping appf v
-          where appf = fromMaybe (exprTyping v) $ M.lookup n _scope
-        app x           = Right x
+    _scope <- traverse inferFromDB . U.universe $ r ^. ruleTail
+    let app v = applyTypeSubst (mconcat _scope) v
         _applied  = U.transformBiM app r
     case _applied of
       Right x -> return x
       Left x  -> throwError $ TypeMismatch x
-  where f :: Expr -> InferenceM ()
-        f (Pred _ n xs) =
-          do
-            dp <- use istProg
-            let ext :: [Rule]
-                ext = extensionalFacts dp
-                matches = L.filter (\x -> ruleName x == n) ext
-            let _args = case L.null matches of
-                  True  -> []
-                  False -> head matches ^. ruleHead . _Pred . _3 
-            let zipped = xs `zip` _args
-            unified <- case traverse (uncurry unifyExprTypes) zipped of
-                              Just x  -> return x
-                              Nothing -> throwError $ TypeMismatch ""
-            let newParams  = xs `zip` unified
-                newParams' = fromMaybe (throw $ TypeMismatch "") $ traverse inferParams newParams
-                unifier :: Typing -> Typing -> Typing
-                unifier a b = fromMaybe (throw $ TypeMismatch "") $ unifyTypings a b
-            istScope . _head %= M.unionWith unifier (M.fromList newParams')
-            return ()
-        f _ = return ()
 
-inferParams :: (Expr, Typing) -> Maybe (String, Typing)
-inferParams (v@(Var e n), x@(Typing _ t)) 
-  | e ^. annBound = (n, exprTyping v) & _2 . tType %%~ unifyTypes t
-  | otherwise     = Just (n, x)
+inferFromDB :: Expr -> InferenceM TypeSubstitution
+inferFromDB (Pred _ n xs) =
+  do
+    dp <- use istProg
+    -- Get the database fact corresponding to the predicate
+    let ext :: [Rule]
+        ext = extensionalFacts dp
+        matches = L.filter (\x -> ruleName x == n) ext
+        _args = case L.null matches of
+          True  -> []
+          False -> head matches ^. ruleHead . _Pred . _3 
+    -- Unify the parameter typings in the database fact and the predicate
+    let zipped = xs `zip` _args
+    unified <- case traverse (uncurry unifyExprTypes) zipped of
+                      Just x  -> return x
+                      Nothing -> throwError $ TypeMismatch ""
+    -- Take bound and unbound variables into account
+    let inferParams :: (Expr, Typing) -> Maybe TypeSubstitution
+        inferParams (v@(Var e n'), x@(Typing _ t)) 
+          | e ^. annBound = (n' |->) <$> (exprTyping v & tType %%~ unifyTypes t)
+          | otherwise     = Just $ n' |-> x
+        newParams  = xs `zip` unified
+        newParams' = traverse inferParams newParams
+    -- Unify the new type substitutions with existing substitutions
+    return (mconcat $ fromMaybe (throw $ TypeMismatch "") newParams')
+inferFromDB _ = return mempty
+
+inferFromGoal :: Expr -> [Rule] -> InferenceM TypeSubstitution
+inferFromGoal g rs =
+  do
+    undefined
+
+inferFromExpr :: InferenceM [Rule]
+inferFromExpr = undefined
 
 applyTyping :: Typing -> Expr -> Either String Expr
 applyTyping (Typing d t) e =
