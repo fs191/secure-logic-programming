49953b77efe119977c83fd11a1928f41c04d0d25
Joosep Jääger <joosep.jaager@gmail.com>
2020-12-29 15:03:02 +0200

    Added expectations

--- examples/ppdatalog/aggregation.pl
+++ examples/ppdatalog/aggregation.pl
@@ -1,14 +1,7 @@
-
-fib(0, 1).
-fib(1, 1).
-
-fib(N, F) :-
-        N > 1,
-        N1 is N-1,
-        N2 is N-2,
-        fib(N1, F1),
-        fib(N2, F2),
-        F is F1+F2.
+% Aggregation test
+less_than(X, Y) :-
+  Y >= 0,
+  X < Y.
 
 :-outputs([Y]).
-?-sum(fib(3,X), X, Y).
+?-sum(less_than(X,5), X, Y).
--- test/SecreCSpec.hs
+++ test/SecreCSpec.hs
@@ -14,7 +14,7 @@ spec = parallel . describe "Language.SecreC" $ do
     compilesSuccessfully "examples/ppdatalog/employee.pl" 4
     compilesSuccessfully "examples/ppdatalog/relatives.pl" 3
     compilesSuccessfully "examples/ppdatalog/fib_unfolded_3_fulltyped.pl" 2
-    compilesSuccessfully "examples/ppdatalog/aggregation.pl" 2
+    compilesSuccessfully "examples/ppdatalog/aggregation.pl" 7
     compilesSuccessfully "examples/ppdatalog/market_unfolded_fulltyped.pl" 4
     compilesSuccessfully "examples/ppdatalog/employee_unfolded_fulltyped.pl" 4
     compilesSuccessfully "examples/ppdatalog/relatives_unfolded_3_fulltyped.pl" 3
@@ -26,22 +26,22 @@ spec = parallel . describe "Language.SecreC" $ do
     compilesSuccessfully "examples/ppdatalog/sqrt.pl" 5
     compilesSuccessfully "examples/ppdatalog/privacy_labels.pl" 5
     compilesSuccessfully "examples/ppdatalog/ship_mintime.pl" 5
-    emulatorGivesCorrectAnswer "examples/ppdatalog/fib.pl" "" 4
+    emulatorGivesCorrectAnswer "examples/ppdatalog/fib.pl" "true\n[2]\n" 4
     emulatorGivesCorrectAnswer "examples/ppdatalog/market.pl" "" 4
-    emulatorGivesCorrectAnswer "examples/ppdatalog/auction.pl" "" 4
-    emulatorGivesCorrectAnswer "examples/ppdatalog/employee.pl" "" 4
-    emulatorGivesCorrectAnswer "examples/ppdatalog/relatives.pl" "" 3
+    emulatorGivesCorrectAnswer "examples/ppdatalog/auction.pl" "true\n[dave]\n[eve]\n" 4
+    emulatorGivesCorrectAnswer "examples/ppdatalog/employee.pl" "true\n[bob,0]\n[henry,1200]\n[victor,1900]\n[alice,2000]\n[peggy,2100]\n" 4
+    emulatorGivesCorrectAnswer "examples/ppdatalog/relatives.pl" "true\n[dave]\n[chris]\n" 3
     emulatorGivesCorrectAnswer "examples/ppdatalog/fib_unfolded_3_fulltyped.pl" "" 2
-    emulatorGivesCorrectAnswer "examples/ppdatalog/aggregation.pl" "" 2
+    emulatorGivesCorrectAnswer "examples/ppdatalog/aggregation.pl" "" 7
     emulatorGivesCorrectAnswer "examples/ppdatalog/market_unfolded_fulltyped.pl" "" 4
-    emulatorGivesCorrectAnswer "examples/ppdatalog/employee_unfolded_fulltyped.pl" "" 4
+    emulatorGivesCorrectAnswer "examples/ppdatalog/employee_unfolded_fulltyped.pl" "true\n[bob,0]\n[henry,1200]\n[victor,1900]\n[alice,2000]\n[peggy,2100]\n" 4
     emulatorGivesCorrectAnswer "examples/ppdatalog/relatives_unfolded_3_fulltyped.pl" "" 3
     emulatorGivesCorrectAnswer "examples/ppdatalog/ship.pl" "" 15
     emulatorGivesCorrectAnswer "examples/ppdatalog/ship_short.pl" "" 5
-    emulatorGivesCorrectAnswer "examples/ppdatalog/precendence.pl" "" 5
+    emulatorGivesCorrectAnswer "examples/ppdatalog/precendence.pl" "true\n[0.0]\n" 5
     emulatorGivesCorrectAnswer "examples/ppdatalog/disjunction.pl" "" 5
     emulatorGivesCorrectAnswer "examples/ppdatalog/nondet.pl" "" 5
-    emulatorGivesCorrectAnswer "examples/ppdatalog/sqrt.pl" "" 5
+    emulatorGivesCorrectAnswer "examples/ppdatalog/sqrt.pl" "true\n[0.0]\n" 5
     emulatorGivesCorrectAnswer "examples/ppdatalog/privacy_labels.pl" "" 5
     emulatorGivesCorrectAnswer "examples/ppdatalog/ship_mintime.pl" "" 5
 
