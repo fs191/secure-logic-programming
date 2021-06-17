f7d5a7770ba6f322a12d47ddecb7844ef8a8c066
Alisa <alisa@cyber.ee>
2021-06-17 16:28:58 +0300

    Returned back some interesting simplifications of inequalities into Simplify.hs. Constrained their signature to integers to avoid type conflicts.

--- src/Translator/Simplify.hs
+++ src/Translator/Simplify.hs
@@ -11,6 +11,7 @@ import Data.Generics.Uniplate.Operations as U
 import qualified Data.Map as M
 import qualified Data.List as L
 import Data.Text.Prettyprint.Doc
+import Data.Maybe
 
 import Expr
 import Substitution
@@ -250,10 +251,8 @@ simplifyExpr (Is a x y) m = (is eq, m')
     is (Eq _ l r) = Is a l r
     is e = e
 
-{-
-TODO add support for non-int datatype for full integration
-
-simplifyExpr e@(Eq _ x y) m
+--TODO add support for non-int datatypes (eq for strings, all inequalities for floats)
+simplifyExpr e@(Eq _ x@(ConstInt _ _) y@(ConstInt _ _)) m
   | (ex == ey && isJust ex) || x == y = (eTrue, m)
   | isNothing bounds = (eFalse, m)
   | otherwise = (simplifyConst e, m'')
@@ -264,7 +263,7 @@ simplifyExpr e@(Eq _ x y) m
     m' = if isVar x then M.insert (_varName x) (fromJust bounds) m else m
     m'' = if isVar y then M.insert (_varName y) (fromJust bounds) m' else m'
 
-simplifyExpr e@(Neq _ x y) m
+simplifyExpr e@(Neq _ x@(ConstInt _ _) y@(ConstInt _ _)) m
   | ex /= ey && isJust ex && isJust ey = (eTrue, m)
   | isNothing bounds = (eFalse, m)
   | otherwise = (simplifyConst e, m'')
@@ -279,9 +278,149 @@ simplifyExpr (Lt a x y) m = lessThan True a x y m
 simplifyExpr (Le a x y) m = lessThan False a x y m
 simplifyExpr e@Gt {} m = (switch lt, m') where (lt, m') = simplifyExpr (switch e) m
 simplifyExpr e@Ge {} m = (switch le, m') where (le, m') = simplifyExpr (switch e) m
--}
 simplifyExpr e m = (simplifyConst e, m)
 
+-- | Checks that given bounds do not contradict each other
+legal :: Bounds -> Bool
+legal (l, e, g, n) = checkLt && checkEq && checkGt
+  where
+    checkLt = isNothing l || (min' l e == l && min' (add l (Just 1)) g /= g)
+    checkEq = isNothing e || notElem (fromJust e) n
+    checkGt = isNothing g || isNothing e || (min' g e == e)
+
+min' :: Maybe Int -> Maybe Int -> Maybe Int
+min' (Just x) (Just y) = Just $ min x y
+min' x y = if isNothing x then y else x
+
+max' :: Maybe Int -> Maybe Int -> Maybe Int
+max' (Just x) (Just y) = Just $ max x y
+max' x y = if isNothing x then y else x
+
+-- | Helper funtion that simplifies an inequality
+lessThan :: Bool -> Ann -> Expr -> Expr -> Env -> (Expr, Env)
+lessThan strict ann x y m
+  | isJust ex && isJust ey && fromJust ex < fromJust ey + fromEnum strict = (eTrue, m)
+  | isNothing xb' || isNothing yb' = (eFalse, m)
+  | otherwise = (e, m'')
+  where
+    e = simplifyConst $ if strict then Lt ann x y else Le ann x y
+    xb@(_, ex, _, _) = if isConstExpr x then intBounds x else getBounds x m
+    yb@(_, ey, _, _) = if isConstExpr y then intBounds y else getBounds y m
+    xb' = if strict then ltBounds xb yb else leBounds xb yb
+    yb' = if strict then gtBounds yb xb else geBounds yb xb
+    m' = if isVar x then M.insert (_varName x) (fromJust xb') m else m
+    m'' = if isVar y then M.insert (_varName y) (fromJust yb') m' else m'
+
+-- | Switches left and right hand sides of an expression
+switch :: Expr -> Expr
+switch (Eq a l r) = Eq a r l
+switch (Lt a l r) = Gt a r l
+switch (Gt a l r) = Lt a r l
+switch (Le a l r) = Ge a r l
+switch (Ge a l r) = Le a r l
+switch e = e
+
+-- | Tries to construct bounds for an (arithmetic) expression
+getBounds :: Expr -> Env -> Bounds
+getBounds (Var _ x) m =
+  if M.member x m
+    then m M.! x
+    else (Nothing, Nothing, Nothing, [])
+getBounds c@(ConstInt _ _) m = intBounds c
+getBounds (Add _ x y) m =
+  (add (add lx ly) (Just 1), add ex ey, sub (add gx gy) (Just 1), n)
+  where
+    (lx, ex, gx, nx) = getBounds x m
+    (ly, ey, gy, ny) = getBounds y m
+    n = (if isJust ex then [a + (fromJust ex) | a <- ny] else [])
+         `L.union` (if isJust ey then [a + (fromJust ey) | a <- nx] else [])
+         `L.union` [a + b | a <- nx, b <- ny]
+getBounds (Sub _ x y) m =
+  (add (sub lx gy) (Just 1), sub ex ey, sub (sub gx ly) (Just 1), [a - b | a <- nx, b <- ny])
+  where
+    (lx, ex, gx, nx) = getBounds x m
+    (ly, ey, gy, ny) = getBounds y m
+    n = (if isJust ex then [a + (fromJust ex) | a <- ny] else [])
+         `L.union` (if isJust ey then [a + (fromJust ey) | a <- nx] else [])
+         `L.union` [a + b | a <- nx, b <- ny]
+getBounds (Mul _ x y) m
+  | isJust ex && isJust ey = intBounds $ constInt (fromJust ex * fromJust ey)
+  | elem Nothing [lx, gx, ly, gy] = (Nothing, Nothing, Nothing, [a * b | a <- nx, b <- ny])
+  | otherwise = (Just l, e, Just g, [a * b | a <- nx, b <- ny])
+  where
+    (lx, ex, gx, nx) = getBounds x m
+    (ly, ey, gy, ny) = getBounds y m
+    (lx', ly', gx', gy') = (fromJust lx + 1, fromJust ly + 1, fromJust gx - 1, fromJust gy - 1)
+    mul = [lx' * ly', lx' * gy', gx' * gy', gx' * ly']
+    l = (L.minimum mul) - 1
+    g = (L.maximum mul) + 1
+    e = if g - l == 2 then Just $ l + 1 else Nothing
+getBounds (Div _ x y) m
+  | isJust ex && isJust ey = intBounds $ constInt (fromJust ex `div` fromJust ey)
+  | otherwise = (Nothing, Nothing, Nothing, [])
+  where
+    (_, ex, _, _) = getBounds x m
+    (_, ey, _, _) = getBounds y m
+
+getBounds _ _ = (Nothing, Nothing, Nothing, [])
+
+
+add :: Maybe Int -> Maybe Int -> Maybe Int
+add (Just x) (Just y) = Just $ x + y
+add _ _ = Nothing
+
+sub :: Maybe Int -> Maybe Int -> Maybe Int
+sub (Just x) (Just y) = Just $ x - y
+sub _ _ = Nothing
+
+-- | Adjusts bounds for the left hand side of a "less than" expression
+ltBounds :: Bounds -> Bounds -> Maybe Bounds
+ltBounds (lx, ex, gx, nx) (ly, ey, gy, _) = if checkEq && legal bounds then Just bounds else Nothing
+  where
+    checkEq = isNothing ex || isNothing ey || ex < ey
+    --l' = max' lx (sub ly (Just 1))
+    l' = lx
+    g' = if isJust ey then min' gx ey else min' gx (sub gy (Just 1))
+    e' = if sub l' g' == Just 2 then add l' (Just 1) else ex
+    bounds = (l', e', g', nx)
+
+-- | Adjusts bounds for the left hand side of a "greater than" expression
+gtBounds :: Bounds -> Bounds -> Maybe Bounds
+gtBounds (lx, ex, gx, nx) (ly, ey, gy, _) = if checkEq && legal bounds then Just bounds else Nothing
+  where
+    checkEq = isNothing ex || isNothing ey || ex > ey
+    l' = if isJust ey then max' lx ey else max' lx (add ly (Just 1))
+    --g' = min' gx (add gy (Just 1))
+    g' = gx
+    e' = if sub l' g' == Just 2 then add l' (Just 1) else ex
+    bounds = (l', e', g', nx)
+
+-- | Constructs common bounds for an "equal" expression
+eqBounds :: Bounds -> Bounds -> Maybe Bounds
+eqBounds (lx, ex, gx, nx) (ly, ey, gy, ny) = if checkEq && legal bounds then Just bounds else Nothing
+  where
+    checkEq = isNothing ex || isNothing ey || ex == ey
+    bounds = (max' lx ly, max' ex ey, min' gx gy, nx `L.union` ny)
+
+-- | Adjusts bounds for the left hand side of a "not equal" expression
+neqBounds :: Bounds -> Bounds -> Maybe Bounds
+neqBounds (lx, ex, gx, nx) (_, ey, _, _) = if checkEq && legal bounds then Just bounds else Nothing
+  where
+    checkEq = isNothing ex || isNothing ey || ex /= ey
+    bounds = (lx, ex, gx, if isJust ey then nx `L.union` [fromJust ey] else nx)
+
+-- | Adjusts bounds for the left hand side of a "less than or equal" expression
+leBounds :: Bounds -> Bounds -> Maybe Bounds
+leBounds x (l, e, g, _) = ltBounds x (add l (Just 1), add e (Just 1), add g (Just 1), [])
+
+-- | Adjusts bounds for the left hand side of a "greater than or equal" expression
+geBounds :: Bounds -> Bounds -> Maybe Bounds
+geBounds x (l, e, g, _) = gtBounds x (sub l (Just 1), sub e (Just 1), sub g (Just 1), [])
+
+-- | Constructs bounds for a constant integer for arithmetic calculations
+intBounds :: Expr -> Bounds
+intBounds (ConstInt _ i) = (Just $ i - 1, Just i, Just $ i + 1, [])
+intBounds e = intBounds $ simplifyConst e
 
 -- | Rewrites constant terms to simpler terms
 simplifyConst :: Expr -> Expr
